3556fe777e5ced1d4966d4362fc7692e0a1a8ac1
Guillermo <duche27@hotmail.com>
2022-03-24 10:08:19 +0100

    primer commit

---
+++ src/main/java/com/example/usersservice/queries/UserEventsHandler.java
@@ -0,0 +1,30 @@
+package com.example.usersservice.queries;
+
+import com.gui.estore.core.model.PaymentDetails;
+import com.gui.estore.core.model.User;
+import com.gui.estore.core.queries.FetchUserPaymentDetailsQuery;
+import org.axonframework.queryhandling.QueryHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserEventsHandler {
+
+    @QueryHandler
+    public User getPaymentDetails(FetchUserPaymentDetailsQuery query) {
+
+        PaymentDetails paymentDetails = PaymentDetails.builder()
+                .cardNumber("123Card")
+                .cvv("123")
+                .name("SERGEY KARGOPOLOV")
+                .validUntilMonth(12)
+                .validUntilYear(2030)
+                .build();
+
+        return User.builder()
+                .firstName("Sergey")
+                .lastName("Kargopolov")
+                .userId(query.getUserId())
+                .paymentDetails(paymentDetails)
+                .build();
+    }
+}
